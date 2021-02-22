00cde8b92348f330403eae8d350f35bde08ab538
AllanCV27 <cahepat27@hotmail.com>
2021-02-22 13:12:14 +0000

    list

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,67 @@
 import React from "react";
-
+import shortid from "shortid";
+import { useState } from "react";
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 export function Home() {
+	const [homeWork, setHomeWork] = useState("");
+	const [allhomeWork, setAllhomeWork] = useState([]);
+	const addHomework = e => {
+		e.preventDefault();
+
+		setAllhomeWork([
+			...allhomeWork,
+			{
+				id: shortid.generate(),
+				namehomeWork: homeWork
+			}
+		]);
+		setHomeWork("");
+	};
+	const deleteHomework = item => {
+		let result = allhomeWork.filter(function(e) {
+			return e.id !== item;
+		});
+		setAllhomeWork(result);
+	};
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="todolist">
+			<h1 className="titulo">TO DO LIST</h1>
+			<div className="form">
+				<form onSubmit={addHomework} className="pizarra">
+					<div className="">
+						<ul className="lihome">
+							{allhomeWork.map(item => (
+								<li className="listawork" key={item.id}>
+									<span className="lead">
+										{item.namehomeWork}
+										<button
+											className="botonbasura"
+											onClick={() => {
+												deleteHomework(item.id);
+											}}>
+											<i className="far fa-trash-alt basura"></i>
+										</button>
+									</span>
+								</li>
+							))}
+						</ul>
+					</div>
+					<input
+						type="text"
+						className="form-control mb-2"
+						placeholder="Add Homework"
+						onChange={e => setHomeWork(e.target.value)}
+						value={homeWork}
+					/>
+					<button
+						className="btn btn-warning btn-lg"
+						type="submit"
+						id="amarillo">
+						ADD
+					</button>
+				</form>
+			</div>
 		</div>
 	);
 }
